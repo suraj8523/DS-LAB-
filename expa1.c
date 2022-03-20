2df276353ece4cc96d0fc9107c96b7f6f6936fa2
surajraj96 <96900923+surajraj96@users.noreply.github.com>
2022-03-20 16:01:52 +0530

    Update expa1.c

--- expa1.c
+++ expa1.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
-int main() {
-  int n;
-  double arr[100];
-  printf("Enter the number of elements (1 to 100): ");
-  scanf("%d", &n);
-
-  for (int i = 0; i < n; ++i) {
-    printf("Enter number%d: ", i + 1);
-    scanf("%lf", &arr[i]);
-  }
-
-  // storing the largest number to arr[0]
-  for (int i = 1; i < n; ++i) {
-    if (arr[0] < arr[i]) {
-      arr[0] = arr[i];
-    }
-  }
-
-  printf("Largest element = %.2lf", arr[0]);
-
-  return 0;
+ 
+int main()
+{
+ 
+        int size, i, largest;
+ 
+        printf("\n Enter the size of the array: ");
+        scanf("%d", &size);
+        int array[size];
+ 
+        printf("\n Enter %d elements of  the array: \n", size);
+ 
+        for (i = 0; i < size; i++)
+        {   
+            scanf("%d", &array[i]);
+        }
+ 
+        largest = array[0];
+ 
+        for (i = 1; i < size; i++) 
+        {
+            if (largest < array[i])
+            largest = array[i];
+        }
+ 
+        printf("\n largest element present in the given array is : %d", largest);
+ 
+        return 0;
+ 
 }
